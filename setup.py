e17aa9758f127ad0420e4fc4681254d725c6ae53
alanpt <ALANPT@GMAIL.COM>
2024-07-04 16:59:31 +1200

    Update setup.py
    Changed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mkdocs-pagelist-plugin',
-    version='0.1.0',
+    version='0.2.0',
     description='A MkDocs plugin to list pages based on tags and folders',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
